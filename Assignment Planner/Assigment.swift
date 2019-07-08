dff030450e73eb2589372e00c24426f5c4b6efb2
Devesh Bhor <deveshbhor@outlook.com>
2019-07-08 17:17:42 -0500

    Created Custom Class

---
+++ Assignment Planner/Assigment.swift
@@ -0,0 +1,26 @@
+//
+//  Assigment.swift
+//  Assignment Planner
+//
+//  Created by Devesh Bhor on 7/8/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class Assigment: Codable {
+
+    var name : String
+    var subject : String
+    var dueDate : String
+    var description : String
+    
+    init(name: String, subject : String, dueDate : String, description: String) {
+        
+        self.name = name
+        self.subject = subject
+        self.dueDate = dueDate
+        self.description = description
+    }
+    
+}
